5f0ad182dd88a76cf4b6ffa9c2cbaae54a520666
Manoj Kg <kgmanoj19@gmail.com>
2023-07-16 18:35:50 +0530

    Final Project (react)

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,10 +1,12 @@
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button, Container, Header, Segment, Image } from "semantic-ui-react";
+import { Button, Container, Header, Segment, Image, Divider } from "semantic-ui-react";
 import { useStore } from '../../app/stores/store';
 import LoginForm from '../users/LoginForm';
 import RegisterForm from '../users/RegisterForm';
+import FacebookLogin from '@greatsumini/react-facebook-login';
+import { resolve } from 'path';
 
 export default observer(function HomePage() {
     const { userStore, modalStore } = useStore();
@@ -29,6 +31,22 @@ export default observer(function HomePage() {
                         </Button>
                         <Button onClick={() => modalStore.openModal(<RegisterForm />)} size='huge' inverted>
                             Register
+                        </Button>
+                        <Divider horizontal inverted>Or</Divider>
+                        <Button
+                            as={FacebookLogin}
+                            appId='613537424204562'
+                            size = 'huge'
+                            inverted
+                            color='facebook'
+                            content='Login with FaceBook'
+                            onSuccess = {(response:any) => {
+                                console.log('Login success', response)
+                            }}
+                            onFail = {(response:any) => {
+                                console.log('Login failed' , response)
+                            }}
+                            >
                         </Button>
                     </>
 
